07d6275a388a1599cd04a46b5a4614fa9364913e
Jeongsu Yang <mycat83@gmail.com>
2022-01-21 21:57:41 +0900

    test: 인수 테스트 공통 클래스 생성

---
+++ kidslife-api/src/test/java/info/kidslife/AcceptanceTest.java
@@ -0,0 +1,70 @@
+package info.kidslife;
+
+import info.kidslife.utils.DatabaseCleanup;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@EnableJpaAuditing
+public abstract class AcceptanceTest {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+        databaseCleanup.execute();
+    }
+
+    public static ExtractableResponse<Response> get(String uri, Object... params) {
+        return given()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get(uri, params)
+                .then().log().all()
+                .extract();
+    }
+
+    public static <T> ExtractableResponse<Response> post(String uri, T body, Object... params) {
+        return given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(body)
+                .when().post(uri, params)
+                .then().log().all()
+                .extract();
+    }
+
+    public static <T> ExtractableResponse<Response> put(String uri, T body, Object... params) {
+        return given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(body)
+                .when().put(uri, params)
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> delete(String uri, Object... params) {
+        return given()
+                .when().delete(uri, params)
+                .then().log().all()
+                .extract();
+    }
+
+    private static RequestSpecification given() {
+        return RestAssured
+                .given().log().all();
+    }
+}
