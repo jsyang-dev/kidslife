f85f636e10d5b5a09ffebfa6cd290f232c899805
Jeongsu Yang <mycat83@gmail.com>
2022-02-06 22:21:53 +0900

    refactor(user): 테스트 리팩터링
    - steps 클래스 추출
    - 미사용 메서드 삭제

--- kidslife-api/src/test/java/info/kidslife/AcceptanceTest.java
+++ kidslife-api/src/test/java/info/kidslife/AcceptanceTest.java
@@ -2,15 +2,12 @@ package info.kidslife;
 
 import info.kidslife.utils.DatabaseCleanup;
 import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -30,40 +27,7 @@ public abstract class AcceptanceTest {
 //        databaseCleanup.execute();
     }
 
-    public static ExtractableResponse<Response> get(String uri, Object... params) {
-        return givenSpec()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().get(uri, params)
-                .then().log().all()
-                .extract();
-    }
-
-    public static <T> ExtractableResponse<Response> post(String uri, T body, Object... params) {
-        return givenSpec()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(body)
-                .when().post(uri, params)
-                .then().log().all()
-                .extract();
-    }
-
-    public static <T> ExtractableResponse<Response> put(String uri, T body, Object... params) {
-        return givenSpec()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(body)
-                .when().put(uri, params)
-                .then().log().all()
-                .extract();
-    }
-
-    public static ExtractableResponse<Response> delete(String uri, Object... params) {
-        return givenSpec()
-                .when().delete(uri, params)
-                .then().log().all()
-                .extract();
-    }
-
-    private static RequestSpecification givenSpec() {
+    protected static RequestSpecification givenSpec() {
         return RestAssured
                 .given().log().all();
     }
